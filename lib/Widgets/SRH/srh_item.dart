d547462b164aa804ccde8d46c6f81ca3c3ed6674
edenAbdisa <eden.abdisa@yahoo.com>
2021-12-30 19:31:40 +0300

    restructure

---
+++ lib/Widgets/SRH/srh_item.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/material/colors.dart';
+
+class SRHItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              height: 150.0,
+              width: 120.0,
+              color: Colors.transparent,
+              child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.orangeAccent[400],
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  child: const Center(
+                    // ignore: unnecessary_const
+                    child: Text("Rounded Corner Rectangle Shape",
+                        style: TextStyle(color: Colors.white, fontSize: 22),
+                        textAlign: TextAlign.center),
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
