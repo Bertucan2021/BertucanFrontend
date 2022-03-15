7235638fcd05dfbbe712756dbe4db838762f5f9d
minas528 <minalem.5d@gmail.com>
2022-03-15 16:00:29 +0300

    fix bugs

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme:
           ThemeData(primarySwatch: Colors.pink, fontFamily: 'Poppins Medium'),
       onGenerateRoute: PageRouter.generateRoute,
-      initialRoute: LoginPage.pageRoute,
+      initialRoute: '/',
     );
   }
 }
