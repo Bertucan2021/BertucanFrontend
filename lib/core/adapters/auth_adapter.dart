d81af97957e99c59d312c02b37225b33a3d358f7
Gelila Mamo <gelilambiresaw@gmail.com>
2022-07-03 22:18:38 +0300

    clean up for restart

--- lib/core/adapters/auth_adapter.dart
+++
@@ -1,7 +0,0 @@
-import 'package:bertucanfrontend/core/models/freezed_models.dart';
-
-abstract class IAuthRepository {
-  Future<void> signUp(UserToSignUp signUpPayload);
-  Future<void> signIn(UserToLogin loginPayload);
-  Future<void> logOut();
-}
