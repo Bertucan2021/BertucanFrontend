6f5e18a4fd9f14d869c4cd5bbb90e5b172661efb
Gelila Mamo <gelilambiresaw@gmail.com>
2022-07-31 11:20:05 +0300

    UI lockscreen update

--- lib/ui/pages/auth/lock_screen_page.dart
+++ lib/ui/pages/auth/lock_screen_page.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:bertucanfrontend/shared/routes/app_routes.dart';
+import 'package:bertucanfrontend/shared/themes/app_theme.dart';
 import 'package:bertucanfrontend/ui/controllers/auth_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screen_lock/flutter_screen_lock.dart';
@@ -16,6 +17,30 @@ class LockScreenPage extends StatelessWidget {
       didUnlocked: () {
         Get.offAndToNamed(Routes.homePage);
       },
+      screenLockConfig: ScreenLockConfig(
+        backgroundColor: Colors.grey,
+      ),
+      secretsConfig: SecretsConfig(
+          spacing: 15, // or spacingRatio
+          padding: const EdgeInsets.all(40),
+          secretConfig: SecretConfig(
+            disabledColor: Colors.grey,
+            enabledColor: AppTheme.primaryColor,
+            height: 15,
+            width: 15,
+          )),
+      keyPadConfig: KeyPadConfig(
+        buttonConfig: StyledInputConfig(
+          textStyle: StyledInputConfig.getDefaultTextStyle(context).copyWith(
+            color: AppTheme.primaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+          buttonStyle: OutlinedButton.styleFrom(
+            backgroundColor: AppTheme.primaryColor,
+          ),
+        ),
+      ),
+      cancelButton: const Icon(Icons.edit, color: Colors.black),
     );
   }
 }
